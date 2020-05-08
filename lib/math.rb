18ecaa2fa10acdc010293947ab17c06659e7e4ac
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-08 18:03:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,18 +5,21 @@ end
 
 def subtraction(num1, num2)
   difference = num1-num2
+  difference
 end
 
 def division(num1, num2)
-  quotient = num1/num2
+  quotient = num1  num2
+  quotient
 end
 
 def multiplication(num1, num2)
   product = num1*num2 
+  product
 end
 
 def modulo(num1, num2)
-
+  answer = num1 % num2
 end
 
 def square_root(num)
