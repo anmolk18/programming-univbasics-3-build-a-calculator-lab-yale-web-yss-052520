58c39e6423b8e8745156e1574f9f9dbbacbc3d11
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-08 18:01:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,18 @@
 def addition(num1, num2)
-  sum = num1 + num2 
+  sum = num1+num2 
   sum
 end
 
 def subtraction(num1, num2)
-  difference = num1 - num2
+  difference = num1-num2
 end
 
 def division(num1, num2)
-  product = num1 * num2
+  product = num1/num2
 end
 
 def multiplication(num1, num2)
-
+  
 end
 
 def modulo(num1, num2)
